8c856d6dbd85269989af898662aea7c4fd5b9b2a
mayank <mayankrajput40558@gmail.com>
2023-09-12 20:31:56 +0530

    feat: firebase auth

--- app/src/main/java/com/example/chatmessenger/LoginActivity.kt
+++ app/src/main/java/com/example/chatmessenger/LoginActivity.kt
@@ -1,23 +1,49 @@
 package com.example.chatmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
 import com.example.chatmessenger.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityLoginBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        auth = Firebase.auth
 
         binding.button.setOnClickListener {
             val username = binding.username.text.toString()
             val password = binding.password.text.toString()
             Toast.makeText(this,"username $username and password is $password",Toast.LENGTH_LONG).show()
+            auth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Hare Krishna", "signInWithEmail:success")
+                        val user = auth.currentUser
+                        Toast.makeText(this,"Auth Successful",Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Hare Krishna", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
         }
     }
 }
--- app/src/main/java/com/example/chatmessenger/MainActivity.kt
+++ app/src/main/java/com/example/chatmessenger/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.chatmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.chatmessenger.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        var auth = Firebase.auth
+        val user = auth.currentUser
+        binding.userview.text = user?.email.toString()
     }
 }
